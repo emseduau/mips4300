4f348c011149940b7342fe29ba3e6589dcb55aa5
emseduau <ems0025@auburn.edu>
2016-10-13 10:05:10 -0500

    Submitted code

---
+++ eSus/gprSim/mipsDataTypes.h
@@ -0,0 +1,6 @@
+#ifndef MIPSDATATYPES_H_INCLUDED
+#define MIPSDATATYPES_H_INCLUDED
+typedef int32_t mem_addr;
+typedef int32_t mem_word;
+typedef int32_t reg_word;
+#endif // MIPSDATATYPES_H_INCLUDED
